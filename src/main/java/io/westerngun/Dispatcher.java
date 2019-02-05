3f9eec9403e9f1471d32dfbe35a2addfc2fc8cd1
WesternGun <westernguncyber@gmail.com>
2019-02-05 17:07:16 +0100

    Impossible to import SWT in Gradle, only can download them. The page is: https://download.eclipse.org/eclipse/downloads/drops4/R-4.10-201812060815#SWT. Change version to 4.10.

--- src/main/java/io/westerngun/Dispatcher.java
+++ src/main/java/io/westerngun/Dispatcher.java
@@ -8,11 +8,11 @@ public class Dispatcher {
     public static void main(String[] args) {
         Runnable runnable = (() -> {
             Display display = new Display();
-            Shell shell = new Shell(display);
+            Shell shell = new Shell(display); // perfectly supports two monitors(can cross-monitor )
             shell.setMinimumSize(new Point(500, 500));
             shell.setSize(new Point(700, 700));
             shell.setMaximized(false);
-            shell.setCapture(false);
+            shell.setCapture(false); // if "true", cursor will be "halting" in the app window in Win10
             shell.setText("Sudoku Maze by WesternGun");
             shell.open();
             while (!shell.isDisposed()) {
